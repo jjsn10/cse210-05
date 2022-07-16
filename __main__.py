116c9158bf869def8687eeaa295c64ba0025117d
jjsn10 <san14038@byui.edu>
2022-07-16 10:16:38 -0600

    main changed

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from game.casting.cast import Cast
 from game.casting.food import Food
 from game.casting.score import Score
 from game.casting.snake import Snake
+from game.casting.cycle import Cycle
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
@@ -21,8 +22,11 @@ def main():
     # create the cast
     cast = Cast()
     cast.add_actor("foods", Food())
-    cast.add_actor("snakes", Snake())
+    #cast.add_actor("snakes", Snake())
+    cast.add_actor("cycles",Cycle(constants.RED))
+    cast.add_actor("cycles",Cycle(constants.BLUE))
     cast.add_actor("scores", Score())
+
    
     # start the game
     keyboard_service = KeyboardService()
